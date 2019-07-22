c337882a2eace6ebb9b3b8f764b9ceb1d6218479
aoczkowicz <andrzej.oczkowicz@thulium.pl>
2019-07-22 08:37:09 +0200

    Add travis config

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 set -eo pipefail
 
+echo "Installing NuGet packages..."
+nuget restore
+
 echo "Building application..."
 msbuild /t:restore
 msbuild /p:Configuration=Release BusylightRestHost.sln /p:TargetFrameworkVersion=v4.7 /target:BusylightRestHost
